3a39aff04129b5131bc38eee2bc44af1d4855012
Jerydam <jerydan148@gmail.com>
2023-03-23 01:15:36 +0100

    auction

--- contracts/facets/OwnershipFacet.sol
+++ contracts/facets/OwnershipFacet.sol
@@ -5,11 +5,12 @@ import { LibDiamond } from "../libraries/LibDiamond.sol";
 import { IERC173 } from "../interfaces/IERC173.sol";
 
 contract OwnershipFacet is IERC173 {
+    uint public age;
     function transferOwnership(address _newOwner) external override {
         LibDiamond.enforceIsContractOwner();
         LibDiamond.setContractOwner(_newOwner);
     }
-
+function age()public{}
     function owner() external override view returns (address owner_) {
         owner_ = LibDiamond.contractOwner();
     }
